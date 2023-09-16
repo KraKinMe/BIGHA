84f32eb3bc31e6f58a6ab3bab1fea37ac4fda262
pushpaksinghal <pushpaksinghal1234@gmail.com>
2023-09-17 04:02:35 +0530

    link register page

--- app/src/main/java/com/example/myapplication/regActivity.kt
+++ app/src/main/java/com/example/myapplication/regActivity.kt
@@ -2,14 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class signUpActivity : AppCompatActivity() {
+class regActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-    }
-    fun onRegisterNowClicked(view: View) {
-      setContentView(R.layout.activity_reg)
+        setContentView(R.layout.activity_reg)
     }
 }
