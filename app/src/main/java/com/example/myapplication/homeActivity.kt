898b7447b7e523c8690946cd1997a6f80bfe67cd
Parth Seth <parthseth2004@gmail.com>
2023-09-17 18:37:41 +0530

    Andar ghusna allowed

--- app/src/main/java/com/example/myapplication/homeActivity.kt
+++ app/src/main/java/com/example/myapplication/homeActivity.kt
@@ -2,10 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
 
 class homeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        window.statusBarColor = Color.rgb(0,0,0)
     }
 }
