f27c7c6abd94617abc3229a31dc0c5e2ce46d3bb
Dhruv-A21 <dhruvagrawal13021307@gmail.com>
2023-09-21 04:30:22 +0530

    Pop

--- app/src/main/java/com/example/myapplication/homeActivity.kt
+++ app/src/main/java/com/example/myapplication/homeActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.MenuItem
 import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -26,6 +31,35 @@ class homeActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val btnKnowMoreMicroFin : Button = findViewById(R.id.btnKnowMoreMicroFin)
+
+        btnKnowMoreMicroFin.setOnClickListener {
+            val FName:String?="MICRO FINANCE"
+            val FExplain:String?="Here Farmers can do Stock level shit (Baadme Explain karna)"
+            showCustomDialogBox(FName,FExplain)
+        }
+
+    }
+
+    private fun showCustomDialogBox(FName: String?,FExplain:String?) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.layout_know_more)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val FeatureName : TextView =dialog.findViewById(R.id.FeatureName)
+        val FeatureExplain : TextView =dialog.findViewById(R.id.FeatureExplain)
+        val btnBackKM : Button = dialog.findViewById(R.id.btnBackKM)
+
+        FeatureName.text=FName
+        FeatureExplain.text=FExplain
+
+        btnBackKM.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
 
     }
 
