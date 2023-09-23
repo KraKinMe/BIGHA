5c9085fe37b80ad6a8a7d521afcd62d656462a33
Parth Seth <parthseth2004@gmail.com>
2023-09-23 15:27:20 +0530

    Upload Image to db , Key being UserID

---
+++ app/src/main/java/com/example/myapplication/Machines.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+data class Machines(val desc:String?=null , val image: String?=null , val name:String?=null , val price:String?= null , val Location:String?= null){
+}
