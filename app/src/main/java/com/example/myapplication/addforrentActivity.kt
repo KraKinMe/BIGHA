5623bf622cfd4447b0bd084b89b818aaaa5e4e47
Parth Seth <parthseth2004@gmail.com>
2023-09-23 13:47:56 +0530

    Upload Image

--- app/src/main/java/com/example/myapplication/addforrentActivity.kt
+++ app/src/main/java/com/example/myapplication/addforrentActivity.kt
@@ -7,14 +7,28 @@ import android.view.View
 import android.widget.ImageView
 import android.app.Activity
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.provider.MediaStore
+import android.util.Base64
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.io.ByteArrayOutputStream
+
 
 class addforrentActivity : AppCompatActivity() {
     private val REQUEST_IMAGE_PICK=1
     private lateinit var toolImageView: ImageView
     private lateinit var selectImageButton: Button
+    private lateinit var Desc:EditText
+    private lateinit var Name:EditText
+    private lateinit var ToolPic:ImageView
+    private lateinit var submit:Button
+    private lateinit var database: DatabaseReference
+    var sImage:String? =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
@@ -26,6 +40,25 @@ class addforrentActivity : AppCompatActivity() {
             // Launch the image picker when the button is clicked
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, REQUEST_IMAGE_PICK)
+
+        }
+        Desc = findViewById(R.id.Descoftool)
+        Name = findViewById(R.id.ToolName)
+        submit = findViewById(R.id.submitBtn)
+        submit.setOnClickListener {
+            Toast.makeText(this,"NOOB",Toast.LENGTH_SHORT).show()
+            val descTxt=Desc.text.toString()
+            val nameTxt=Name.text.toString()
+            database = FirebaseDatabase.getInstance().getReference("Machines")
+            val Machines=Machines(descTxt,sImage,nameTxt)
+
+       //     Toast.makeText(this,"Agla toast",Toast.LENGTH_SHORT).show()
+            database.child(nameTxt).setValue(Machines).addOnSuccessListener {
+                    Toast.makeText(this,"Your tool is up for Rent",Toast.LENGTH_SHORT).show()
+
+            }.addOnCanceledListener {
+                Toast.makeText(this,"Not done Bro",Toast.LENGTH_SHORT).show()
+            }
         }
     }
     fun previous(view : View){
@@ -42,6 +75,14 @@ class addforrentActivity : AppCompatActivity() {
             try {
                 val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImageUri)
                 toolImageView.setImageBitmap(bitmap)
+                val inputStream = contentResolver.openInputStream(selectedImageUri!!)
+                val myBitmap = BitmapFactory.decodeStream(inputStream)
+                var stream = ByteArrayOutputStream()
+                myBitmap.compress(Bitmap.CompressFormat.PNG,100,stream)
+                var bytes = stream.toByteArray()
+                sImage= Base64.encodeToString(bytes,Base64.DEFAULT)
+                Toast.makeText(this,"Image Selected",Toast.LENGTH_SHORT).show()
+
             } catch (e: Exception) {
                 e.printStackTrace()
             }
