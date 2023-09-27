e6596cbfdba9a863dd246016bedee593ea801242
Dhruv-A21 <dhruvagrawal13021307@gmail.com>
2023-09-28 00:29:52 +0530

    Disease Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,4 @@ dependencies {
     implementation ("androidx.camera:camera-camera2:1.1.0-alpha02")
     implementation ("androidx.camera:camera-lifecycle:1.1.0-alpha02")
     implementation ("androidx.camera:camera-view:1.0.0-alpha30")
-
 }
