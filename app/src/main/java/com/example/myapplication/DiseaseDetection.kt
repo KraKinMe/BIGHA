456f5dd59565f51b082c8844cd2f99396e6a5ba5
Dhruv-A21 <dhruvagrawal13021307@gmail.com>
2023-09-28 00:36:44 +0530

    Disease Page

--- app/src/main/java/com/example/myapplication/DiseaseDetection.kt
+++ app/src/main/java/com/example/myapplication/DiseaseDetection.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
 /**
  * A simple [Fragment] subclass.
  * Use the [DiseaseDetection.newInstance] factory method to
